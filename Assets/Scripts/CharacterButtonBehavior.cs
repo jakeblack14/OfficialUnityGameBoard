fd246c365ae70dd005f74c532d9a985ecb906854
Lab <ablack3@harding.edu>
2018-03-17 14:57:03 -0500

    First Commit

---
+++ Assets/Scripts/CharacterButtonBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CharacterButtonBehavior : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
+{
+    private Button pb;
+    public Sprite newSprite;
+    public Sprite oldSprite;
+
+    void Start()
+    {
+        pb = GetComponent<Button>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        pb.image.sprite = newSprite;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        pb.image.sprite = oldSprite;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
